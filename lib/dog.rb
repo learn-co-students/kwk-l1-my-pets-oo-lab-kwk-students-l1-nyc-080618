fa0038422d0bda3e4fe9d1dce53a0edf479411aa
graciemzhang <gracie.zhang@lps-students.org>
2018-08-10 15:28:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-  # code goes here
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  
+  def name
+    return @name
+  end
+  
+  def mood
+    return @mood
+  end
+  
+  def mood=(new_mood)
+    @mood = new_mood
+  end
 end
